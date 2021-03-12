b732a7213a9d64c9df8aef04db2b7f8a3fc84e1c
latikak <latikakorad@gmail.com>
2021-03-12 15:13:19 -0500

    REF: Renamed updatePlayerLocation and made it private

--- src/lib/CoveyTownController.ts
+++ src/lib/CoveyTownController.ts
@@ -132,7 +132,7 @@ export default class CoveyTownController {
    * @param player Player to update location for
    * @param location New location for this player
    */
-  updatePlayerLocation(player: Player, location: UserLocation): void {
+  private onPlayerMovement(player: Player, location: UserLocation): void {
     player.updateLocation(location);
     this._listeners.forEach((listener) => listener.onPlayerMoved(player));
   }
@@ -186,7 +186,7 @@ export default class CoveyTownController {
     // Register an event listener for the client socket: if the client updates their
     // location, inform the CoveyTownController
     socket.on('playerMovement', (movementData: UserLocation) => {
-      this.updatePlayerLocation(session.player, movementData);
+      this.onPlayerMovement(session.player, movementData);
     });
   }
 }
